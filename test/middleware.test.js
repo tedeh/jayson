014cd579850263323c9f1ecb2ab1052e934a3105
Tedde Lundgren <mail@tedeh.net>
2012-02-01 20:05:31 +0100

    Fixed a pretty crappy logic error and added tests for middleware

--- test/middleware.test.js
+++ test/middleware.test.js
@@ -6,11 +6,13 @@ var express = require('express');
 
 var serverMethods = {
   add: function(a, b, callback) { callback(null, a + b); },
+  divide: function(a, b, callback) { callback(null, a / b); },
   error: function(callback) { callback(this.error(-1000, 'An error message')); }
 };
 
 describe('The Express HTTP server', function() {
   var server = null;
+
   it('can be successfully set up to listen on localhost:3000', function(done) {
     (function() {
       server = express.createServer();
@@ -35,6 +37,7 @@ describe('A HTTP Server', function() {
 
   describe('and a HTTP Client', function() {
     var client = jayson.client.http({ host: 'localhost', port: 3000 });
+
     it('should successfully be able to request "add" on the server', function(done) {
       client.request('add', [11, 12], function(err, error, response) {
         should.not.exist(err);
@@ -45,6 +48,19 @@ describe('A HTTP Server', function() {
         done();
       });
     });
+
+    it('should successfully be able to request "divide" with named parameters', function(done) {
+      var params = {b: 3, a: 12};
+      client.request('divide', params, function(err, error, response) {
+        should.not.exist(err);
+        should.not.exist(error);
+        should.exist(response);
+        response.should.be.a('number');
+        response.should.equal(params.a / params.b);
+        done();
+      });
+    });
+
     it('should successfully be able to receive a deliberate error from the server', function(done) {
       client.request('error', [], function(err, error, response) {
         should.not.exist(err);

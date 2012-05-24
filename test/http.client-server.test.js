ec69a739c2ee70a0be152fc2883287704bbca1c1
Tedde Lundgren <mail@tedeh.net>
2012-05-24 22:37:08 +0200

    Improve http client and server tests

--- test/http.client-server.test.js
+++ test/http.client-server.test.js
@@ -1,64 +1,66 @@
-var assert = require('assert');
 var should = require('should');
-
 var jayson = require(__dirname + '/..');
 
-var serverMethods = {
-  add: function(a, b, callback) { callback(null, a + b); },
-  error: function(callback) {
-    callback(this.error(-1000, 'An error message'));
-  }
-};
+describe('jayson http', function() {
 
-describe('The HTTP server', function() {
   var server = null;
-  it('can be successfully set up to listen on localhost:3000', function(done) {
-    (function() {
-      server = jayson.server(serverMethods).http();
-      server.listen(3000, done);
-    }).should.not.throw();
+
+  var methods = {
+    add: function(a, b, callback) { callback(null, a + b); },
+    error: function(callback) { callback(this.error(-1000, 'An error message')); }
+  };
+
+  describe('server', function() {
+
+    it('should listen to a local port', function(done) {
+      (function() {
+        server = jayson.server(methods).http();
+        server.listen(3000, 'localhost', done);
+      }).should.not.throw();
+    });
+
+    it('should be an instance of http.Server', function() {
+      server.should.be.instanceof(require('http').Server);
+    });
+
   });
 
-  after(function(done) {
-    server.on('close', done);
-    server.close();
-  });
-});
+  describe('client', function() {
+    
+    var client = jayson.client.http({
+      host: 'localhost',
+      port: 3000
+    });
 
-describe('A HTTP Server', function() {
-  var server = null;
-  before(function(done) {
-    server = jayson.server(serverMethods).http();
-    server.listen(3000, done);
-  });
+    it('should be an instance of jayson.client', function() {
+      client.should.be.instanceof(jayson.client);
+    });
 
-  describe('and a HTTP Client', function() {
-    var client = jayson.client.http({ host: 'localhost', port: 3000 });
-    it('should successfully be able to request "add" on the server', function(done) {
-      client.request('add', [11, 12], function(err, error, response) {
-        should.not.exist(err);
-        should.not.exist(error);
-        should.exist(response);
-        response.should.be.a('number');
-        response.should.equal(11 + 12);
+    it('should be able to request a method on the server', function(done) {
+      var a = 11, b = 12;
+      client.request('add', [a, b], function(err, error, result) {
+        if(err || error) return next(err || error);
+        should.exist(result);
+        result.should.equal(a + b);
         done();
       });
     });
-    it('should successfully be able to receive a deliberate error from the server', function(done) {
-      client.request('error', [], function(err, error, response) {
-        should.not.exist(err);
-        should.not.exist(response);
-        should.exist(error, error.message, error.code);
-        error.should.be.a('object');
-        error.message.should.be.a('string').and.equal('An error message');
-        error.code.should.be.a('number').and.equal(-1000);
+
+    it('should be able to receive an error from the server', function(done) {
+      client.request('error', [], function(err, error, result) {
+        if(err) return done(err);
+        should.not.exist(result);
+        should.exist(error, error.code, error.message);
+        error.message.should.equal('An error message');
+        error.code.should.equal(-1000);
         done();
       });
     });
+
   });
 
-  after(function(done) {
-    server.on('close', done);
+  after(function() {
     server.close();
   });
+
 });

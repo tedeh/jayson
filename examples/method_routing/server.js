253d4358a60fb685b2e5583bb9a9245ed42ef140
Tedde Lundgren <mail@tedeh.net>
2015-01-02 16:15:19 +0100

    Convert method routing example to new method definition

--- examples/method_routing/server.js
+++ examples/method_routing/server.js
@@ -12,7 +12,10 @@ var server = jayson.server(methods, {
   router: function(method) {
     // regular by-name routing first
     if(typeof(this._methods[method]) === 'function') return this._methods[method];
-    if(method === 'add_2') return this._methods.add.bind(this, 2);
+    if(method === 'add_2') {
+      var fn = server.getMethod('add').getHandler();
+      return jayson.Method(fn.bind(null, 2));
+    }
   }
 });
 

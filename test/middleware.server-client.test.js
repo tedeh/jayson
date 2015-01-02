4f904836b1700c394afbc50a7f4b619714109f50
Tedde Lundgren <mail@tedeh.net>
2015-01-02 12:29:01 +0100

    Add options.end in Server.middleware and corresponding test

--- test/middleware.server-client.test.js
+++ test/middleware.server-client.test.js
@@ -6,7 +6,8 @@ var connect = require('connect');
 
 describe('jayson middleware', function() {
 
-  var server = connect.createServer();
+  var app = connect.createServer();
+  var server = null; // set in before()
   var client = jayson.client.http({
     reviver: support.server.options.reviver,
     replacer: support.server.options.replacer,
@@ -15,9 +16,9 @@ describe('jayson middleware', function() {
   });
 
   before(function(done) {
-    server.use(connect.json({reviver: support.server.options.reviver}));
-    server.use(jayson.server(support.server.methods, support.server.options).middleware());
-    server = server.listen(3000, done);
+    app.use(connect.json({reviver: support.server.options.reviver}));
+    app.use(jayson.server(support.server.methods, support.server.options).middleware());
+    server = app.listen(3000, done);
   });
 
   after(function() {
@@ -25,5 +26,31 @@ describe('jayson middleware', function() {
   });
 
   describe('common tests', common(client));
+
+  describe('end false', function() {
+
+    before(function() {
+      // change last connect stack handle to one with new options
+      app.stack[app.stack.length - 1].handle = jayson.server(support.server.methods, support.server.options).middleware({
+        end: false
+      });
+    });
+
+    it('should support passing to the next middleware', function(done) {
+      var invocations = 0;
+      app.use(function(req, res, next) {
+        invocations++;
+        res.end();
+      });
+      client.request('add', [3, 4], function(err, error, response) {
+        if(err) throw err;
+        if(error) throw error;
+        response.should.eql(3 + 4);
+        invocations.should.eql(1);
+        done();
+      });
+    });
+  
+  });
 
 });

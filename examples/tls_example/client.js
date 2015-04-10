a4c3c76602b3900b8a75acf68565ce47bd40ca9c
Brian Dunlay <brian@surround.io>
2015-04-10 16:52:21 -0700

    added TLS support

---
+++ examples/tls_example/client.js
@@ -0,0 +1,24 @@
+var jayson = require(__dirname + '/../..');
+var fs = require('fs')
+var path = require('path')
+
+// Read node's tls documentation for more information about these options:
+// https://nodejs.org/api/tls.html#tls_tls_connect_options_callback
+var options = {
+  key: fs.readFileSync(path.resolve(__dirname + '../../../test/fixtures/keys/agent1-key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname + '../../../test/fixtures/keys/agent1-cert.pem')),
+
+  // This is necessary only if the client uses the self-signed certificate.
+  ca: [ fs.readFileSync(path.resolve(__dirname + '../../../test/fixtures/keys/ca1-cert.pem')) ],
+  port: 3000,
+  host: 'localhost'
+};
+
+// create a client
+var client = jayson.client.tls(options);
+
+// invoke "add"
+client.request('add', [1, 1], function(err, error, response) {
+  if(err) throw err;
+  console.log(response); // 2!
+});
---
+++ test/tls.client-server.test.js
@@ -0,0 +1,81 @@
+var should = require('should');
+var fs = require('fs');
+var jayson = require(__dirname + '/..');
+var support = require('./support');
+var common = support.common;
+var JSONStream = require('JSONStream');
+var tls = require('tls');
+
+var serverOptions = {
+  ca: [fs.readFileSync('./test/fixtures/keys/ca1-cert.pem')],
+  key: fs.readFileSync('./test/fixtures/keys/agent1-key.pem'),
+  requestCert: true,
+  cert: fs.readFileSync('./test/fixtures/keys/agent1-cert.pem')
+};
+
+describe('Jayson.Tls', function() {
+
+  describe('server', function() {
+
+    var server = null;
+
+    after(function() {
+      server.close();
+    });
+
+    it('should listen to a local port', function(done) {
+        server = jayson.server(support.methods, support.options).tls(serverOptions);
+        server.listen(3000, 'localhost', done);
+    });
+
+    it('should be an instance of tls.Server', function() {
+      server.should.be.instanceof(tls.Server);
+    });
+
+  });
+
+  describe('client', function() {
+    
+    var server = jayson.server(support.server.methods, support.server.options);
+    var server_tls = server.tls(serverOptions);
+    var client = jayson.client.tls({
+      reviver: support.server.options.reviver,
+      replacer: support.server.options.replacer,
+      host: 'localhost',
+      port: 3000,
+      ca: serverOptions.ca
+    });
+
+    before(function(done) {
+      server_tls.listen(3000, 'localhost', done);
+    });
+
+    after(function() {
+      server_tls.close();
+    });
+
+    describe('common tests', common(client));
+
+    it('should send a parse error for invalid JSON data', function(done) {
+      var socket = tls.connect(3000, 'localhost', serverOptions, function() {
+        var response = JSONStream.parse();
+
+        response.on('data', function(data) {
+          data.should.containDeep({
+            id: null,
+            error: {code: -32700} // Parse Error
+          });
+          socket.end();
+          done();
+        });
+
+        socket.pipe(response);
+
+        // obviously invalid
+        socket.write('abc');
+      });
+    });
+
+  });
+
+});

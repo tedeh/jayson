2ce1297ecdc6263e9fbe0e7f330a82450b3a6bd5
Tedde Lundgren <mail@tedeh.net>
2020-05-28 13:34:31 +0700

    work on ws implementation

---
+++ test/ws.client-server.test.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const should = require('should');
+const jayson = require('./../');
+const support = require('./support');
+const suites = require('./support/suites');
+const http = require('http');
+const url = require('url');
+
+describe('jayson.ws', function() {
+
+  const server = jayson.server(support.server.methods(), support.server.options());
+  const serverWs = server.ws();
+  const client = jayson.client.ws({
+    reviver: support.server.options().reviver,
+    replacer: support.server.options().replacer,
+    // host: 'localhost',
+    // port: 3999
+  });
+
+  before(function(done) {
+    serverWs.listen(3999, 'localhost', done);
+  });
+
+  after(function() {
+    if(serverWs) serverWs.close();
+  });
+
+  describe('server', function() {
+
+    it('should be an instance of http.Server', function() {
+      serverHttp.should.be.instanceof(http.Server);
+    });
+
+    describe('common http server tests', suites.getCommonForHttpServer(server, client));
+
+    it('should not crash when given invalid JSON', function(done) {
+      const reqOptions = {
+        hostname: 'localhost',
+        port: 3999,
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+      };
+      const req = http.request(reqOptions);
+      req.end('invalid json', 'utf8');
+      req.on('response', function(res) {
+        let body = '';
+        res.on('data', function(chunk) { body += chunk; });
+        res.on('end', function() {
+          res.statusCode.should.equal(400)
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('client', function() {
+
+    it('should accept a URL string as the first argument', function() {
+      const urlStr = 'http://localhost:3999';
+      const client = jayson.client.http(urlStr);
+      const tokens = url.parse(urlStr);
+      client.options.should.containDeep(tokens);
+    });
+
+    describe('common tests', suites.getCommonForClient(client));
+
+    describe('common http client tests', suites.getCommonForHttpClient(client));
+
+  });
+
+});

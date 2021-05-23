9cedfedd137f72b956457573d65d2f006e80b732
Tedde Lundgren <mail@tedeh.net>
2021-05-23 15:10:53 +0700

    fix example websocket client

--- examples/websocket/client.js
+++ examples/websocket/client.js
@@ -1,10 +1,13 @@
 const WebSocket = require('isomorphic-ws');
 const jayson = require('../../');
 
-const ws = new WebSocket('ws://localhost:12345');
+const client = jayson.client.websocket({
+  url: 'ws://localhost:12345',
+});
 
-const client = jayson.client.websocket(ws);
-
-client.request('add', [1,2,3,4], function (err, result) {
-  console.log(err, result);
+client.ws.on('open', function () {
+  client.request('add', [1,2,3,4], function (err, result) {
+    console.log(err, result);
+    client.ws.close();
+  });
 });
